1d3aee94ef6831763a19c78465a5c077f9e6f0ae
RVP97 <rovapin@gmail.com>
2022-10-28 18:21:28 -0500

    added more buttons

--- convert.js
+++ convert.js
@@ -9,6 +9,8 @@ var alternatingText = document.getElementById("alternating-text");
 var copy = document.getElementById("copy");
 var binary = document.getElementById("binary");
 var binaryToText = document.getElementById("binary-to-text");
+var reverse = document.getElementById("reverse");
+var susbscript = document.getElementById("subscript");
 
 upperCase.addEventListener("click", function () {
   text.value = text.value.toUpperCase();
@@ -85,4 +87,30 @@ binaryToText.addEventListener("click", function () {
     binString += String.fromCharCode(parseInt(bin, 2));
   });
   text.value = binString;
+});
+
+// hide binary to text button if the text is not binary
+text.addEventListener("keyup", function () {
+  if (text.value.match(/^[0-1\s]+$/)) {
+    binaryToText.style.display = "block";
+  } else {
+    binaryToText.style.display = "none";
+  }
+});
+
+reverse.addEventListener("click", function () {
+  text.value = text.value.split("").reverse().join("");
+});
+
+// convert text into superscript
+susbscript.addEventListener("click", function () {
+  var textArray = text.value.toUpperCase().split("");
+  var superscript = "₀₁₂₃₄₅₆₇₈₉";
+  var superscriptArray = superscript.split("");
+  for (var i = 0; i < textArray.length; i++) {
+    if (textArray[i] >= "0" && textArray[i] <= "9") {
+      textArray[i] = superscriptArray[textArray[i]];
+    }
+  }
+  text.value = textArray.join("");
 });
